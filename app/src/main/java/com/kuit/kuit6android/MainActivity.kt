0efaab1e07aaa96c928fe5d60c8d5c5b42bc1669
ProtossManse <ingan2473@gmail.com>
2025-09-18 17:51:19 +0900

    Init: 안드로이드 프로젝트 생성 및 gitignore 내용 추가

---
+++ app/src/main/java/com/kuit/kuit6android/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kuit.kuit6android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kuit.kuit6android.ui.theme.KUIT6_ANDROIDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KUIT6_ANDROIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KUIT6_ANDROIDTheme {
+        Greeting("Android")
+    }
+}
