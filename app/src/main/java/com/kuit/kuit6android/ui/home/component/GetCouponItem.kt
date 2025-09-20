03736db5a71f30fc9acfbdbb9e05fd4d73e97f88
Zach <dnfjddk2@gmail.com>
2025-09-20 14:43:41 +0900

    Feat: 홈 화면 UI 추가 (검색창, 쿠폰, 카테고리)

---
+++ app/src/main/java/com/kuit/kuit6android/ui/home/component/GetCouponItem.kt
@@ -0,0 +1,89 @@
+package com.kuit.kuit6android.ui.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuit.kuit6android.R
+
+@Composable
+fun GetCouponItem(modifier: Modifier = Modifier) {
+    var isWhite by remember { mutableStateOf(value = true) }
+    LazyRow(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color(color = 0xFF212B35))
+            .padding(horizontal = 15.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = "img_twinkle",
+                modifier = modifier.size(width = 19.dp, height = 16.dp)
+            )
+        }
+        item {
+            Text(
+                text = "무료배달 + 총 10,000원 할인",
+                modifier = modifier.padding(start = 5.dp),
+                color = Color(color = 0xFFFDFFFF),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        item {
+            Text(
+                text = "적용 가능",
+                modifier = modifier.padding(start = 2.dp, end = 43.dp),
+                color = Color(color = 0xFFFDFFFF),
+                fontSize = 12.sp
+            )
+        }
+        item {
+            Button(
+                modifier = modifier
+                    .padding(top = 5.dp, end = 7.dp, bottom = 8.dp, start = 9.dp),
+                onClick = {
+                    isWhite = !isWhite
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(color = 0xFF02AFFE),
+                    contentColor = if (isWhite) Color.White else Color.Black
+                ),
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                Text(
+                    text = "쿠폰 받기",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CouponPreview() {
+    GetCouponItem()
+}
