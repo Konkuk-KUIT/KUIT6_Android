76618a34d2d428756e399c0776ca730fd5543c99
BearYeWon <bear100152@gmail.com>
2025-09-23 19:07:14 +0900

    CategoryItem LazyColumn 구현 추가

--- app/src/main/java/com/kuit/kuit6android/ui/home/component/CategoryItem.kt
+++ app/src/main/java/com/kuit/kuit6android/ui/home/component/CategoryItem.kt
@@ -2,8 +2,10 @@ package com.kuit.kuit6android.ui.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,19 +18,24 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun CategoryItem(modifier: Modifier = Modifier, categoryData: CategoryData) {
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.height(67.dp)
     ) {
-        Image(
-            painter = painterResource(categoryData.imageId),
-            contentDescription = categoryData.category,
-            modifier = modifier.size(47.dp)
-        )
-        Text(
-            text = categoryData.category,
-            color = Color.Black,
-            fontSize = 11.sp,
-            modifier = modifier.padding(top = 4.dp)
-        )
+        item {
+            Image(
+                painter = painterResource(categoryData.imageId),
+                contentDescription = categoryData.category,
+                modifier = modifier.size(47.dp)
+            )
+
+            Text(
+                text = categoryData.category,
+                color = Color.Black,
+                fontSize = 11.sp,
+                modifier = modifier.padding(top = 4.dp)
+            )
+        }
+
     }
 }
