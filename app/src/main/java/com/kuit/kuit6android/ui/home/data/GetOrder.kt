08d8a04543eee8e0758d2cf2411a1205f20944c2
hyhyh <hyeonjiana@google.com>
2025-09-26 18:29:07 +0900

    2주차 실습 과제

---
+++ app/src/main/java/com/kuit/kuit6android/ui/home/data/GetOrder.kt
@@ -0,0 +1,6 @@
+package com.kuit.kuit6android.ui.home.data
+
+data class GetOrder(
+    val imageId : Int,
+    val category: String
+)
--- app/src/main/java/com/kuit/kuit6android/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/kuit/kuit6android/ui/home/screen/HomeScreen.kt
@@ -2,21 +2,64 @@ package com.kuit.kuit6android.ui.home.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kuit.kuit6android.ui.home.component.CategoryRow
+import com.kuit.kuit6android.ui.home.component.GetCouponItem
+import com.kuit.kuit6android.ui.home.component.Location
+import com.kuit.kuit6android.ui.home.component.OrderGoGo
+import com.kuit.kuit6android.ui.home.component.RecentMatjib
+import com.kuit.kuit6android.ui.home.component.SearchTextField
 
 @Composable
 fun HomeScreen(
     padding: PaddingValues,
     modifier: Modifier = Modifier
 ) {
+//    LazyColumn (
+//
+//    ) {
+        //랜더이슈가 발생해서 lazycolunm으로 구현을 못했습니다
+//        item{ GetCouponItem() }
+//        item{ Location() }
+//        item{ SearchTextField(modifier = modifier.padding(start = 49.dp)) }
+//        item{ CategoryRow(modifier = modifier.fillMaxWidth()) }
+//        item{ CategoryRow(modifier = modifier.fillMaxWidth()) }
+//        item { OrderGoGo() }
+//        item { RecentMatjib() }
+//
+    //   }
     Column(
-        modifier = Modifier.padding(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
             padding
         )
     ) {
-        Text("Home Screen")
+        GetCouponItem()
+        Location()
+        SearchTextField(modifier = modifier.padding(start = 49.dp))
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CategoryRow(modifier = modifier.fillMaxWidth())
+            CategoryRow(modifier = modifier.fillMaxWidth())
+        }
+        OrderGoGo()
+        RecentMatjib()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun homePreview() {
+    HomeScreen(PaddingValues())
 }
