6583c635ec0b2b157d34c5ea3ac088588cd52650
woornjs <tonyrnjs12e@naver.com>
2025-09-28 01:19:13 +0900

    Merge remote-tracking branch 'origin/develop' into develop

---
+++ app/src/main/java/com/kuit/kuit6android/ui/home/component/Advertise.kt
@@ -0,0 +1,87 @@
+package com.kuit.kuit6android.ui.home.component
+
+import android.bluetooth.le.AdvertiseData
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Advertise(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 24.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .size(width = 306.dp, height = 77.dp)
+                .clip(shape = RoundedCornerShape(8.dp))
+                .background(color = Color(0xFF0C2F65))
+                .padding(start = 81.dp, end = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(space = 18.dp)
+        ) {
+            Column {
+                Text(
+                    text = "와우회원은 횟수 제한없이",
+                    color = Color.White,
+                    fontSize = 11.sp,
+                )
+                Text(
+                    text = "매 주문 무료배달",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .size(width = 84.dp, height = 30.dp),
+                colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(4.dp),
+                contentPadding = PaddingValues(
+                    start = 6.dp,
+                    top = 6.dp,
+                    end = 6.dp,
+                    bottom = 6.dp
+                )
+            ) {
+                Text(
+                    text = "주문하러 가기",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF0C2F65)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AdvertisePreview() {
+    Advertise()
+}
---
+++ app/src/main/java/com/kuit/kuit6android/ui/home/component/recent.kt
@@ -0,0 +1,88 @@
+package com.kuit.kuit6android.ui.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuit.kuit6android.R
+
+@Composable
+fun recent(modifier: Modifier = Modifier) {
+    Text(
+        text = "최근 본 맛집",
+        fontSize = 17.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier.padding(start = 25.dp)
+    )
+
+    LazyRow(
+        modifier = modifier.padding(start = 25.dp, top = 450.dp),
+        horizontalArrangement = Arrangement.spacedBy(21.dp)
+    ) {
+        item {
+            Column(
+                modifier = modifier.padding(end = 27.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.image),
+                    contentDescription = "최근 식당",
+                    modifier = modifier
+                        .padding(top = 25.dp)
+                        .width(230.dp)
+                        .height(129.dp)
+                )
+            }
+
+            Row(
+                modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "아워 떡볶이",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "30분",
+                    fontSize = 12.sp
+                )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.star),
+                    contentDescription = "찜",
+                    modifier = modifier.size(15.dp)
+                )
+                Text(
+                    text = "4.9 (3,849)",
+                    fontSize = 12.sp,
+                    color = Color(0xFF575252)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun recentPreview() {
+    recent()
+}
