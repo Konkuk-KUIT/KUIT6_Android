8e50c628de3c8bb286dd280e08fe55aabacc3b38
Zach <dnfjddk2@gmail.com>
2025-09-27 22:45:23 +0900

    Feat: 검색 화면 및 검색 결과 화면 UI 구현, 네비게이션 로직 추가
    - `SearchBar` 컴포저블을 추가하여 검색 화면과 검색 결과 화면에서 공통으로 사용합니다.
    - `SearchScreen`에서 인기 검색어 목록을 표시하고, 각 항목 클릭 시 검색 결과 화면(`SearchResultScreen`)으로 이동하도록 네비게이션 로직을 구현했습니다.
    - `SearchResultScreen`은 전달받은 검색어를 `SearchBar`에 표시합니다.
    - 뒤로 가기 아이콘(`ic_back_arrow.xml`)을 추가했습니다.
    - `MainNavHost`에 검색 관련 화면을 위한 중첩된 네비게이션 그래프(`SearchNestedGraphRoute`)를 추가하고, `SearchScreen`과 `SearchResultScreen`을 연결했습니다.

---
+++ app/src/main/java/com/kuit/kuit6android/ui/search/screen/SearchResultScreen.kt
@@ -0,0 +1,23 @@
+package com.kuit.kuit6android.ui.search.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kuit.kuit6android.ui.search.components.SearchBar
+
+@Composable
+fun SearchResultScreen(
+    padding: PaddingValues,
+    searchKeyword: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.padding(paddingValues = padding)
+    ) {
+        SearchBar(
+            text = searchKeyword
+        )
+    }
+}
